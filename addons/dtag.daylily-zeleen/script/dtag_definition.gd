3a02d64c7e6739b26f7e53ee2eb769182a22033e
忘忧の <daylily-zeleen@foxmail.com>
2025-10-14 16:53:24 +0800

    Finish generate tool.

--- addons/dtag.daylily-zeleen/script/dtag_definition.gd
+++ addons/dtag.daylily-zeleen/script/dtag_definition.gd
@@ -3,7 +3,7 @@ class_name DTagDefinition
 # Example：
 # const TestNamespace = {
 # 	Scope1 = {
-# 		Tag1 = "This tag will become &"TestNamesapce.Tag", # Any String/StringName will be recognized as Tag, and text content will become comment comment of thsi tag in generated script.
+# 		Tag1 = "This tag will become &'TestNamesapce.Tag'", # Any String/StringName will be recognized as Tag, and text content will become comment comment of thsi tag in generated script.
 # 		Tag2 = null, # Use "null" to mark a tag.
 # 	},
 # 	Tag3 = "",
--- addons/dtag.daylily-zeleen/tool/tool_generate_dtag.gd
+++ addons/dtag.daylily-zeleen/tool/tool_generate_dtag.gd
@@ -5,20 +5,44 @@ var gen_target_file := "res://dtag.gen.gd"
 
 
 func _run() -> void:
+	var processed_scripts: Array[Script]
+
 	var gd_script_files := _get_gd_scripts_recursively()
 	var tag_definitions: Dictionary[String, Dictionary] = {}
 	for f in gd_script_files:
 		var script := ResourceLoader.load(f, "GDScript", ResourceLoader.CACHE_MODE_IGNORE) as Script
-		_extract_tag_definitions(script, tag_definitions)
+		_extract_tag_definitions(script, processed_scripts, tag_definitions)
 
-	var text := """# This file is generated, any modify maybe discard.
-class_name DTag
+	# Generate
+	var identifiers: PackedStringArray
+	var text := "# NOTE: This file is generated, any modify maybe discard.\n"
+	text += "class_name DTag\n\n"
+	for tag_namespace in tag_definitions:
+		text += "\n"
+		text += "const %s = {\n" % tag_namespace
+		text += _generate_tag_text_recursively(tag_definitions[tag_namespace], tag_namespace, identifiers)
+		text += "}\n"
 
-"""
-	
+	var class_list := ClassDB.get_class_list()
+	var global_class_list := ProjectSettings.get_global_class_list().map(func(c: Dictionary) -> String: return c.class )
+	for identifier in identifiers:
+		if class_list.has(identifier) or global_class_list.has(identifier):
+			print_rich("[color=yellow][WARN] DTag: tag \"%s\" is a class name.[/color]")
+
+	var fa := FileAccess.open(gen_target_file, FileAccess.WRITE)
+	if not is_instance_valid(fa):
+		printerr("[ERROR] DTag: open \"%s\" failed, %s." % [gen_target_file, error_string(FileAccess.get_open_error())])
+		return
+
+	fa.store_string(text)
+	fa.close()
+
+	EditorInterface.get_resource_filesystem().update_file(gen_target_file)
+	print("DTag: \"%s\" is generated." % [gen_target_file])
 
 
-func _get_gd_scripts_recursively(base_dir := "res://", r_files:PackedStringArray=[]) -> PackedStringArray:
+#region Extract Definitions
+func _get_gd_scripts_recursively(base_dir := "res://", r_files: PackedStringArray = []) -> PackedStringArray:
 	if base_dir == "res://addons/":
 		return r_files
 
@@ -42,16 +66,37 @@ func _get_gd_scripts_recursively(base_dir := "res://", r_files:PackedStringArray
 	return r_files
 
 
-func _extract_tag_definitions(script: GDScript, r_tag_definitions: Dictionary[String, Dictionary]) -> Dictionary[String, Dictionary]:
-	var cosntant_map := script.get_script_constant_map()
+# Return value format example: {
+# 	Namespace1 = {
+# 		Namespace2 = {
+# 			A = "xxx", # Comment
+# 		}
+# 		B = "xxx",
+# 		C = "",
+# 	},
+# 	Namespace3 = {
+# 		A = "",
+# 	},
+# }
+func _extract_tag_definitions(script: GDScript, processed_scripts: Array[Script], r_tag_definitions: Dictionary[String, Dictionary]) -> Dictionary[String, Dictionary]:
+	if processed_scripts.has(script):
+		return r_tag_definitions
 
-	for k in cosntant_map:
+	processed_scripts.push_back(script)
+	
+	var constant_map := script.get_script_constant_map()
+
+	for k in constant_map:
 		if not typeof(k) in [TYPE_STRING, TYPE_STRING_NAME]:
 			continue
-		var v: Variant = cosntant_map.get(k, null)
+		var v: Variant = constant_map.get(k, null)
+		if v is Script:
+			_extract_tag_definitions(script, processed_scripts, r_tag_definitions)
+			continue
+
 		if typeof(v) != TYPE_DICTIONARY:
 			continue
-		if not _is_valid_definistion_dictionary_recursively(v):
+		if not _is_valid_definition_dictionary_recursively(v):
 			continue
 
 		if not r_tag_definitions.has(k):
@@ -76,17 +121,44 @@ func _merge_tag_definition_recursively(ori: Dictionary, dest: Dictionary) -> voi
 				_merge_tag_definition_recursively(ori[k], v)
 
 
-func _is_valid_definistion_dictionary_recursively(dict: Dictionary) -> bool:
+func _is_valid_definition_dictionary_recursively(dict: Dictionary) -> bool:
 	for k in dict:
 		if not typeof(k) in [TYPE_STRING, TYPE_STRING_NAME]:
 			return false
-		var v : Variant = dict[dict]
+		var v: Variant = dict[k]
 		match typeof(v):
 			TYPE_STRING, TYPE_STRING_NAME, TYPE_NIL:
 				pass
 			TYPE_DICTIONARY:
-				if not _is_valid_definistion_dictionary_recursively(v):
+				if not _is_valid_definition_dictionary_recursively(v):
 					return false
 			_:
 				return false
 	return true
+#endregion Extract Definitions
+
+
+#region Generate
+func _generate_tag_text_recursively(definition: Dictionary, prev_tag: String, r_identifiers: PackedStringArray, depth := 0) -> String:
+	var ret := ""
+	for key: String in definition:
+		var next: Variant = definition[key]
+		var tag := "%s.%s" % [prev_tag, key]
+
+		if not r_identifiers.has(key):
+			r_identifiers.push_back(key)
+
+		match typeof(next):
+			TYPE_STRING, TYPE_STRING:
+				var comment: String = next
+				if not comment.is_empty():
+					ret += "\t## %s\n" % comment
+				ret += "\t%s = &\"%s\",\n" % [key, tag]
+			TYPE_DICTIONARY:
+				ret += "\t%s = {\n" % key
+				ret += _generate_tag_text_recursively(next, tag, r_identifiers, depth + 1)
+				ret += "\t},\n"
+	for i in range(depth):
+		ret = ret.indent("\t")
+	return ret
+#endregion Generate
