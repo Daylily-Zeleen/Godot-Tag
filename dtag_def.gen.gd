2c0e7f48db3c538d624e8b5abcfe072699f0928a
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2025-10-24 16:05:05 +0800

    更新示例

--- dtag_def.gen.gd
+++ dtag_def.gen.gd
@@ -2,6 +2,13 @@
 class_name DTagDef
 
 
+## Tag without domain
+const TagWithoutDomain = &"TagWithoutDomain"
+
+## Tag without domain
+const TagWithoutDomain1 = &"TagWithoutDomain1"
+
+
 ## Desc
 const MainDomain = {
 	DOMAIN_NAME = &"MainDomain",
@@ -10,14 +17,33 @@ const MainDomain = {
 	## Desc
 	Domain = {
 		DOMAIN_NAME = &"Redirect.To.New.Domain",
-		## Desc
+		## Will be auto redirect to "Redirect.To.New.Domain.Tag2"
 		Tag2 = &"Redirect.To.New.Domain.Tag2",
-		## Desc
+		## Will be auto redirect to "Redirect.To.New.Domain.Tag3"
 		Tag3 = &"Redirect.To.New.Domain.Tag3",
 	},
 }
 
+## Sample redirect domain.
+const Redirect = {
+	DOMAIN_NAME = &"Redirect",
+	To = {
+		DOMAIN_NAME = &"Redirect.To",
+		New = {
+			DOMAIN_NAME = &"Redirect.To.New",
+				Tag = &"Redirect.To.New.Tag",
+				Domain = {
+					DOMAIN_NAME = &"Redirect.To.New.Domain",
+							Tag1 = &"Redirect.To.New.Domain.Tag1",
+							Tag2 = &"Redirect.To.New.Domain.Tag2",
+							Tag3 = &"Redirect.To.New.Domain.Tag3",
+				},
+		},
+	},
+}
 
+
+# ===== Redirect map. =====
 const _REDIRECT_NAP: Dictionary[StringName, StringName] = {
 	&"MainDomain.Tag1" : &"Redirect.To.New.Tag",
 	&"MainDomain.Domain" : &"Redirect.To.New.Domain",
